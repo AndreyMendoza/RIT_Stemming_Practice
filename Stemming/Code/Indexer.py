df15c6ffb63e94006a4cb210194dca8187327afc
Unknown <andreymendozaf@gmail.com>
2017-10-28 19:12:59 -0600

    Antes de hacer stemming

--- Stemming/Code/Indexer.py
+++ Stemming/Code/Indexer.py
@@ -1,51 +1,98 @@
-import re
 import os
 import sys
-import collections
+import re
+import operator
+from collections import Counter
+from nltk.stem.snowball import SnowballStemmer
+
 
 class Indexer:
 
 # ----------------------------------------------------------------------------------------------------------------------
 
     def __init__(self, collection_path):
-        self.vocabulary = {}
+        self.vocabulary = Counter()
         self.prefixes = {}
         self.collection_path = collection_path
+        self.file_names = []
 
-        self.read_collection()
+        # Execute the logic
+        # self.read_collection()
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-    def read_collection(self):
+    def read_collection(self, n_files=300):
+
+        self.extract_file_names()
+        self.file_names = self.file_names[0:n_files]
+
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                content = file.read()
+                self.process_content(content)
+        self.vocabulary = self.sort_dictionary(self.vocabulary, 0)[0]
+        self.save_vocabulary()
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+    def extract_file_names(self):
+
+        # Extract all the folder names of the collection
         try:
             sub_dir = os.listdir(self.collection_path)
         except:
             sys.exit('El directorio de la coleccion no fue encntrado')
 
+        # Extract file names inside each folder of the collection
         for dir in sub_dir:
             try:
-                file_names = os.listdir(self.collection_path + '\\' + dir)
+                dir_path = self.collection_path + '\\' + dir
+                file_names = os.listdir(dir_path)
+
+                for file_name in file_names:
+                    self.file_names += [dir_path + '\\' + file_name]
             except:
                 continue
-            for file_name in file_names:
-                with open(self.collection_path + '\\' + dir + '\\' + file_name, 'r', encoding='utf-8') as content_file:
-                    content = content_file.read()
-                    self.process_content(content)
-                exit(1)
 
 # ----------------------------------------------------------------------------------------------------------------------
 
     def process_content(self, content):
 
         regex = r'[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+'
+        content = content.lower()
         words = re.findall(regex, content)
-        words_aux = words[0].lower()
-        words[0] = words_aux
+        self.vocabulary += Counter(words)
 
-        count = collections.Counter(words)
+# ----------------------------------------------------------------------------------------------------------------------
+
+    def apply_stemming(self):
+
+        stemmer = SnowballStemmer('spanish')
 
 
+# ----------------------------------------------------------------------------------------------------------------------
+
+    def save_vocabulary(self):
         file = open('Results\\words.txt', 'w')
-        file.write(str(count))
+        file.write(str(self.vocabulary))
+        file.close()
 
-        return 1
+# ----------------------------------------------------------------------------------------------------------------------
+
+    def sort_dictionary(self, dict, value):
+
+        '''
+        Orders a dictionary by it's key or term, depending on the value inserted.
+        :param dict: dictionary to be sorted
+        :param value: wich value use to order the dictionary. 0 is by key and 1 by term.
+        :return: tuple of list of tuples(key:term) and a dictionary object ordered.
+        '''
+
+        sorted_values = sorted(dict.items(), key=operator.itemgetter(value))
+        result_dict = {}
+
+        for tuple in sorted_values:
+            result_dict[tuple[0]] = tuple[1]
+        return result_dict, sorted_values
+
+# ----------------------------------------------------------------------------------------------------------------------
--- Stemming/main.py
+++ Stemming/main.py
@@ -1,11 +1,15 @@
 from Code.Indexer import *
+import time
 
 
 def main():
+    start = time.clock()
+
     collection_path = 'E:\\spanish_billion_words'
     idx = Indexer(collection_path)
-    idx.read_collection()
+    idx.read_collection(1)
 
+    print('Finalizado!\nDuracion: ', (time.clock() - start) / 1, ' minutos.')
 
 main()
 
